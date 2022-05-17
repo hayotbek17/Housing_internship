df5d5a69a214cdd333932fac404cf592bf053934
AlQalb <hayotbekyoldashev2@gmail.com>
2022-05-18 03:24:23 +0500

    home

--- src/components/Home/Carousel/index.jsx
+++ src/components/Home/Carousel/index.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
-import { Container, Img } from './style';
+import React, { useRef } from 'react';
+import { Container, Icon, Img, Wrapper } from './style';
 import img1 from '../../../assets/img/img1.png';
 import img2 from '../../../assets/img/img2.png';
 
 export const Carousel = () => {
+  const slider = useRef();
   return (
-    <Container autoplay>
-      <Img src={img1} />
-      <Img src={img2} />
-      <Img src={img2} />
-      <Img src={img2} />
-    </Container>
+    <Wrapper>
+      <Icon.Left onClick={() => slider.current.prev()} />
+      <Icon.Right onClick={() => slider.current.next()} />
+      <Container ref={slider} dots autoplay>
+        <Img src={img1} />
+        <Img src={img2} />
+        <Img src={img2} />
+        <Img src={img2} />
+      </Container>
+    </Wrapper>
   );
 };
 export default Carousel;
--- src/components/Home/Carousel/style.js
+++ src/components/Home/Carousel/style.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 import { Carousel } from 'antd';
+import { ReactComponent as Arrow } from '../../../assets/icons/ArrowIcon.svg';
 
+const Wrapper = styled.div`
+  position: relative;
+  padding-bottom: 96px;
+`;
 const Container = styled(Carousel)`
   width: 100%;
   height: 570px;
@@ -10,4 +15,55 @@ const Img = styled.img`
   width: 100%;
   height: 570px;
 `;
-export { Container, Img };
+
+const Icon = styled.div``;
+
+Icon.Left = styled(Arrow)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 45px;
+  height: 45px;
+  top: 50%;
+  left: 20px;
+  color: white;
+  background-color: white;
+  padding: 12px;
+  opacity: 0.7;
+  border-radius: 50%;
+  z-index: 999;
+  & path {
+    fill: #0d263b;
+  }
+  :hover {
+    opacity: 0.97;
+  }
+  cursor: pointer;
+`;
+Icon.Right = styled(Arrow)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  width: 45px;
+  height: 45px;
+  top: 50%;
+  right: 20px;
+  transform: rotate(180deg);
+  color: white;
+  background-color: white;
+  padding: 12px;
+  opacity: 0.7;
+  border-radius: 50%;
+  z-index: 999;
+  & path {
+    fill: #0d263b;
+  }
+  :hover {
+    opacity: 0.97;
+  }
+  cursor: pointer;
+`;
+
+export { Container, Img, Icon, Wrapper };
